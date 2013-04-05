3feccebe6eedf54f8dc62096095589560403f8e4
Jamie Longmuir <longmuir@gmail.fake>
2013-04-05 14:50:51 -0400

    Add blog tutorial

---
+++ blog2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog2::Application.config.secret_token = '95ebab7175abd318a122852356ecebf218f8c4e08912f5927e808dbd8c3e3250b0a8505ef31c077d62c9655a013d82f35f99afb3c069ef6d7798cfcac9eb46e6'
